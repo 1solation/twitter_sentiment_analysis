d19fb632a0ee7a3834ba2a32f57bb31a1ce4b205
Hasib Ahmed <hasibahmed1996@gmail.com>
2019-01-29 20:26:04 +0000

    clear search term & edit to console output

--- script.py
+++ script.py
@@ -1,31 +1,39 @@
 import tweepy
 from textblob import TextBlob
 
-#obfuscate keys from code
+# obfuscate keys from code
 lines = []                  # Declare an empty list
-with open ('./keys/consumer_key.txt', 'rt') as in_file:  # Open file ./keys/consumer_keys.txt to enable reading of the data
+with open('./keys/consumer_key.txt', 'rt') as in_file:
+    # Open file ./keys/consumer_keys.txt to enable reading of the data
     for line in in_file:  # loop over each line in in_file
-        lines.append(line.rstrip('\n'))   # add line to list, strip newline chars
+        lines.append(line.rstrip('\n'))  # add to list & strip newline chars
 
 # add key variables - keys file not commited for security reasons.
-consumer_key= lines[0]
-consumer_secret= lines[1]
+consumer_key = lines[0]
+consumer_secret = lines[1]
 
-access_token= lines[2]
-access_token_secret= lines[3]
+access_token = lines[2]
+access_token_secret = lines[3]
+
 # authenticate with twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-#search tweets for any string and store in public_tweets variable
-public_tweets = api.search('Hasib')
+# enter the search term you want to search twitter for and analyse
+search_term = 'brexit'
+
+# search tweets for any string and store in public_tweets variable
+public_tweets = api.search(search_term)
 
 
+print('######################################################################')
+print("### Here is what people are saying about '"
+    + search_term + "' and how they are feeling ###")
+print('######################################################################')
 for tweet in public_tweets:
     print(tweet.text)
     # use TextBlob library to perform sentiment analysis on tweets
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
-    print("")
+    print('\n')
